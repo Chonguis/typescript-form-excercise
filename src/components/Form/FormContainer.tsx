db37d756807ddd6981da71f2b93584f5dde121a9
Chonguis <robertitopacheco@gmail.com>
2020-03-31 23:31:00 -0400

    feat(cascadingeffect): buggy

--- src/components/Form/FormContainer.tsx
+++ src/components/Form/FormContainer.tsx
@@ -1,14 +1,16 @@
 import React, { Component, ChangeEvent } from 'react';
 import './FormContainer.css';
+import { JSXElement } from '@babel/types';
+import { Options } from 'stack-utils';
 
 const cars = [
-  {year:2020, make:'Acura', model:'TLX'},
-  {year:2020, make:'Acura', model:'RDX'},
-  {year:2020, make:'Toyota', model:'Yaris'},
-  {year:2020, make:'Toyota', model:'Corolla'},
-  {year:2019, make:'Toyota', model:'Camry'},
-  {year:2019, make:'BMW', model:'2 SERIES'},
-  {year:2019, make:'BMW', model:'3 SERIES'},
+  {year:"2020", make:'Acura', model:'TLX'},
+  {year:"2020", make:'Acura', model:'RDX'},
+  {year:"2020", make:'Toyota', model:'Yaris'},
+  {year:"2020", make:'Toyota', model:'Corolla'},
+  {year:"2019", make:'Toyota', model:'Camry'},
+  {year:"2019", make:'BMW', model:'2 SERIES'},
+  {year:"2019", make:'BMW', model:'3 SERIES'},
 ];
 const inputValues = [
   {id: 'firstName', label: 'First Name', type: 'text'},
@@ -51,11 +53,57 @@ class Form extends Component<{}, State> {
     }
   }
 
-  onChangeHandler = (event: ChangeEvent<HTMLInputElement>, id: string):void => {
+  onChangeInput = (event: ChangeEvent<HTMLInputElement>, id: string):void => {
     this.setState({
       ...this.state,
       [id]: event.target.value,
     });
+  }
+
+  onChangeSelect = (event: ChangeEvent<HTMLSelectElement>, id: string):void => {
+    this.setState({
+      ...this.state,
+      [id]: event.target.value,
+    });
+  }
+
+  getOptions = (id: string): JSX.Element[] | undefined => {
+    if (id == "year"){
+      let years: string[] = [];
+      cars.map(car => {
+        !years.includes(car.year) && years.push(car.year);
+      }) 
+      let options: JSX.Element[] = [<option value=''></option>];
+      years.map(year => {
+        options.push(<option value={year}>{year}</option>)
+      })
+      if (options) return options;
+    }
+    if (id == "make") {
+      let makers: string[] = [];
+      cars.map(car => {
+        console.log(this.state.year, car.year, '');
+        this.state.year == car.year && !makers.includes(car.make) && makers.push(car.make);  
+      })
+      let options: JSX.Element[] = [];
+      makers.map(make => {
+        options.push(<option value={make}>{make}</option>)
+      })
+      console.log(options);
+      if (options) return options;
+    }
+    if (id == "model") {
+      let models: string[] = [];
+      cars.map(car => {
+        this.state.make == car.make && this.state.year == car.year && models.push(car.model);
+      })
+      let options: JSX.Element[] = [];
+      models.map(model => {
+        options.push(<option value={model}>{model}</option>)
+      })
+      console.log(options);
+      if (options) return options;
+    }
   }
 
   render() {
@@ -64,22 +112,24 @@ class Form extends Component<{}, State> {
     
     let inputHTML = inputValues.map(inputValue => {
       console.log(inputValue.id);
-      !selectors.includes(inputValue.id) ? 
+      if(!selectors.includes(inputValue.id)) {
         inputs.push(
           <div>
               <label htmlFor={inputValue.id}>{inputValue.label}</label>
               <br />
-              <input type={inputValue.type} id={inputValue.id} onChange={(e) => this.onChangeHandler(e, inputValue.id)} /><span>{this.state[inputValue.id]}</span>
+              <input type={inputValue.type} id={inputValue.id} onChange={(e) => this.onChangeInput(e, inputValue.id)} /><span>{this.state[inputValue.id]}</span>
               <br /><br />
           </div>
-        ) : 
+        )
+      } else {
         inputs.push(
           <div>
             <label htmlFor={inputValue.id}>{inputValue.label}</label><br />
-            <select id={inputValue.id} >
-              
+            <select id={inputValue.id} onChange={(e) => this.onChangeSelect(e, inputValue.id)} >
+              {this.getOptions(inputValue.id)}
             </select>
           </div>)
+      }
       })
     console.log('inputArray', inputs);
 
